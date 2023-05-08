9c3caf8700e50914fa63369a692e9c62a76d669a
Diana <gatea.diana@gmail.com>
2023-05-08 10:06:03 +0300

    new version

--- feeding.js
+++ feeding.js
@@ -3,16 +3,17 @@ new Vue({
     data: {
         time: 0,
         timerInterval: null,
-        setTime:null,
+        setTime: null,
         buttonClickTime: null,
         timestored: '',
         startTime: null,
         timerStarted: false,
-        stopTime:null,
-        amount:'',
-        typeMilk:'',
+        stopTime: null,
+        amount: '',
+        typeMilk: '',
         isHidden: false,
         endTime: null,
+        foodsEaten: [],
     },
     mounted() {
         setInterval(() => {
@@ -55,11 +56,31 @@ new Vue({
             this.endTime = new Date();
             this.timerStarted = false;
             this.isHidden = false;
-        }
         },
 
+        addFood() {
+            if (!this.typeMilk || !this.amount) {
+                return;
+            }
+            const existingFoodItem = this.foodsEaten.find(item => item.startsWith(`${this.typeMilk} `));
+            if (existingFoodItem) {
+                // update the existing food item
+                const existingAmount = existingFoodItem.split(' ')[1];
+                const newAmount = parseInt(existingAmount) + parseInt(this.amount);
+                const newFoodItem = `${this.typeMilk} ${newAmount} ml`;
+                const existingItemIndex = this.foodsEaten.indexOf(existingFoodItem);
+                this.foodsEaten.splice(existingItemIndex, 1, newFoodItem);
+            } else {
+                // add a new food item
+                const newFoodItem = `${this.typeMilk} ${this.amount} ml`;
+                this.foodsEaten.push(newFoodItem);
+            }
+            this.typeMilk = '';
+            this.amount = '';
 
 
+        },
+    }
 
 });
 
